c42c01d81b0269bed192765b3e39c4fea6b3ca26
Gerson <gersoncafilho@gmail.com>
2020-01-17 13:52:17 -0300

    modifi baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    //baseURL: 'http://localhost:3333',
+    baseURL: 'https://omnistack10gerssonmg.herokuapp.com/',
 });
 
 export default api;
