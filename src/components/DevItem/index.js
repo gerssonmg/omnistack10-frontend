b93f09e70727e069f41d17a0d08b1bcefe52633d
Gerson <gersoncafilho@gmail.com>
2020-01-21 17:41:03 -0300

    Add function delete() usergithub

--- src/components/DevItem/index.js
+++ src/components/DevItem/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './styles.css';
+import api from '../../services/api';
 
 
 // import { Container } from './styles';
 
 export default function DevItem({ dev }) {
+
+    async function delete_dev(nickDev) {
+
+        if (window.confirm(`Do you really want to remove this usergithub:${nickDev}`)) {
+            const response = await api.delete('/dev',
+                { data: { 'github_username': nickDev } });
+            if (response.data.deletedCount === 1) {
+                alert(`Usuario: ${nickDev}, removido com sucesso`);
+            };
+        }
+    }
+
+
 
     return (
         <li className="dev-item">
@@ -18,8 +32,8 @@ export default function DevItem({ dev }) {
             <p>{dev.bio}</p>
             <>
                 <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil Github </a>
-                <button className="fa fa-edit btn-form"></button>
-                <button className="fa fa-trash btn-form"></button>
+                <button className="fa fa-edit btn-form "></button>
+                <button className="fa fa-trash btn-form" onClick={() => delete_dev(dev.github_username)}></button>
             </>
         </li>
     );
